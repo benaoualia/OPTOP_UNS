24be64bb5c0b849b3f0d17401a71ed908d0ed95c
Optimisation-Pfe <77837016+Optimisation-Pfe@users.noreply.github.com>
2021-01-24 18:19:45 +0100

    Update FE.m

--- FE.m
+++ FE.m
@@ -1,27 +1,21 @@
 %%%%%%%%%% FE-ANALYSIS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function [U]=FE(nelx,nely,x,penal,connective,coord,DDL)
-[KE] = lk; 
-K = sparse(2*(nelx+1)*(nely+1), 2*(nelx+1)*(nely+1));
-F = sparse(2*(nely+1)*(nelx+1),1); U = zeros(2*(nely+1)*(nelx+1),1);
-
-for i=1:nelx*nely
-   edof=sort([DDL(connective(i,:),1) ; DDL(connective(i,:),2)]);
-   K(edof,edof) = K(edof,edof) + x(i)^penal*KE;
+function [U]=FE(N,C,connectiv,x,penal, fixeddofs,F)
+    [KE] = lk; 
+    N2=2*N;
+    K = sparse(N2, N2);
+    F = sparse(N2,1);
+    U = zeros(N2,1);
+    for i=1:C
+        e=sort(connectiv(i,:));
+        edof = [2*e(1)-1; 2*e(1); 2*e(3)-1; 2*e(3);2*e(4)-1; 2*e(4); 2*e(2)-1; 2*e(2)];
+        K(edof,edof) = K(edof,edof) + x(i)^penal*KE;
+    end
+    
+    % DEFINE LOADS AND SUPPORTS (HALF MBB-BEAM)
+    alldofs     = [1:N2];
+    freedofs    = setdiff(alldofs,fixeddofs);
+    
+    % SOLVING
+    U(freedofs,:) = K(freedofs,freedofs) \ F(freedofs,:);      
+    U(fixeddofs,:)= 0;
 end
-
-% for elx = 1:nelx
-%   for ely = 1:nely
-%     n1 = (nely+1)*(elx-1)+ely; 
-%     n2 = (nely+1)* elx   +ely;
-%     edof = [2*n1-1; 2*n1; 2*n2-1; 2*n2; 2*n2+1; 2*n2+2; 2*n1+1; 2*n1+2];
-%     K(edof,edof) = K(edof,edof) + x(ely,elx)^penal*KE;
-%   end
-% end
-% DEFINE LOADS AND SUPPORTS (HALF MBB-BEAM)
-F(2,1) = -1;
-fixeddofs   = union([1:2:2*(nely+1)],[2*(nelx+1)*(nely+1)]);
-alldofs     = [1:2*(nely+1)*(nelx+1)];
-freedofs    = setdiff(alldofs,fixeddofs);
-% SOLVING
-U(freedofs,:) = K(freedofs,freedofs) \ F(freedofs,:);      
-U(fixeddofs,:)= 0;
