5aea11aa06b17ae30c82e1590fdced43c7ff3c8e
Optimisation-Pfe <77837016+Optimisation-Pfe@users.noreply.github.com>
2021-01-24 18:21:14 +0100

    Update check.m

--- check.m
+++ check.m
@@ -1,26 +1,14 @@
 %%%%%%%%%% MESH-INDEPENDENCY FILTER %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function [dcn]=check(nelx,nely,rmin,x,dc,voisin,coord)
-dcn=zeros(nely*nelx,1);
-for i=1:nelx*nely
-    sum=0.0;
-    for j=1:length(voisin{i,1})
-        fac = rmin-sqrt((coord(i,1)-coord(voisin{i,1}(j),1))^2+(coord(i,2)-coord(voisin{i,1}(j),2))^2);
-        sum = sum+max(0,fac);
-        dcn(i) = dcn(i) + max(0,fac)*x(i)*dc(i);
+function [dcn]=check(C,rmin,x,dc,voisins,centers)
+dcn=zeros(C,1);
+for i = 1:C
+    sum=0.0; 
+    voisin=voisins{i};
+    m=length(voisin);
+    for j=1:m
+        fac = rmin-sqrt((centers(i,1)-centers(voisin(j),1))^2+((centers(i,2)-centers(voisin(j),2))^2));
+        sum = sum+fac;
+        dcn(i) = dcn(i) +fac*x(i).*dc(i);
     end
     dcn(i) = dcn(i)/(x(i)*sum);
 end
-
-% for i = 1:nelx
-%   for j = 1:nely
-%     sum=0.0; 
-%     for k = max(i-floor(rmin),1):min(i+floor(rmin),nelx)
-%       for l = max(j-floor(rmin),1):min(j+floor(rmin),nely)
-%         fac = rmin-sqrt((i-k)^2+(j-l)^2);
-%         sum = sum+max(0,fac);
-%         dcn(j,i) = dcn(j,i) + max(0,fac)*x(l,k)*dc(l,k);
-%       end
-%     end
-%     dcn(j,i) = dcn(j,i)/(x(j,i)*sum);
-%   end
-% end
