be55a2c750ec1f97543382cc35e01b2952fca84c
Optimisation-Pfe <77837016+Optimisation-Pfe@users.noreply.github.com>
2021-02-08 10:19:26 +0100

    Update check.m

--- check.m
+++ check.m
@@ -1,15 +1,15 @@
 %%%%%%%%%% MESH-INDEPENDENCY FILTER %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function [dcn]=check(C,rmin,x,dc,voisins,centers,distances)
+function [dcn]=check(C,rmin,x,dc,voisins,distances)
 dcn=zeros(C,1);
 for i = 1:C
     sum=0.0; 
     voisin=voisins{i};
-    distance=distances{i}
+    distance=distances{i};
     m=length(voisin);
     for j=1:m
         fac = rmin-distance(j);
         sum = sum+fac;
-        dcn(i) = dcn(i) +fac*x(j).*dc(j);
+        dcn(i) = dcn(i) +fac*x(voisin(j)).*dc(voisin(j));
       end
     end
     dcn(i) = dcn(i)/(x(i)*sum);
