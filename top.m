7b71b70ced162708e50d45d06d33497723d6628d
Optimisation-Pfe <77837016+Optimisation-Pfe@users.noreply.github.com>
2021-02-08 10:03:00 +0100

    Update top.m

--- top.m
+++ top.m
@@ -1,45 +1,14 @@
-function top(connectiv,volfrac,penal,rmin,nelx,nely,N,voisins,centers,F,fixeddofs)
-% INITIALIZE
-N2=2*N;
-alldofs     = [1:N2];
-freedofs    = setdiff(alldofs,fixeddofs);
-C=length(connectiv); %nombre des éléments
-
-x(1:C) = volfrac; 
-loop = 0; 
-change = 1.;
-
-% START ITERATION
-while change > 0.01  
-      loop = loop + 1;
-      xold = x;
-
-    % FE-ANALYSIS
-      [U]=FE(N2,C,connectiv,x,penal,F,fixeddofs,freedofs);  
-      
-    % OBJECTIVE FUNCTION AND SENSITIVITY ANALYSIS
-      [KE] = lk;
-      c = 0.;
-      for i= 1:C
-            S=connectiv(i,:);
-            Ue = U( [2*S(1)-1; 2*S(1);2*S(2)-1; 2*S(2); 2*S(3)-1; 2*S(3);2*S(4)-1; 2*S(4) ],1);
-            c = c + x(i)^penal*Ue'*KE*Ue;
-            dc(i) = -penal*x(i)^(penal-1)*Ue'*KE*Ue;
-      end
-      
-    % FILTERING OF SENSITIVITIES
-      [dc]   = check(C,rmin,x,dc,voisins,centers);    
-      
-    % DESIGN UPDATE BY THE OPTIMALITY CRITERIA METHOD
-      [x]    = OC(N,x,volfrac,dc); 
-      
-%     % PRINT RESULTS
-      change = max(max(abs(x-xold)));
-      disp([' It.: ' sprintf('%4i',loop) ' Obj.: ' sprintf('%10.4f',c) ...
-           ' Vol.: ' sprintf('%6.3f',sum(sum(x))/(N*volfrac)) ...
-            ' ch.: ' sprintf('%6.3f',change )])
-        
-    % PLOT DENSITIES  
-    xx=reshape(x,[nelx,nely]);
-      colormap(gray); imagesc(-xx); axis equal; axis tight; axis off;pause(1e-6);
-end 
+%%%%%%%%%% FE-ANALYSIS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [U]=FE(N2,C,connectiv,x,penal,F,fixeddofs,freedofs)
+    [KE] = lk;
+    K = sparse(N2, N2);
+    U = zeros(N2,1);
+    for i=1:C
+        e=connectiv(i,:);
+        edof = [2*e(1)-1; 2*e(1); 2*e(2)-1; 2*e(2); 2*e(3)-1; 2*e(3);2*e(4)-1; 2*e(4)];
+        K(edof,edof) = K(edof,edof) + x(i)^penal*KE;
+    end
+    % SOLVING
+    U(freedofs,:) = K(freedofs,freedofs) \ F(freedofs,:);      
+    U(fixeddofs,:)= 0;
+end
