dfcd6bcaad4e6451a5b8579de318207f0578d602
Optimisation-Pfe <77837016+Optimisation-Pfe@users.noreply.github.com>
2021-02-08 09:56:11 +0100

    Update OC.m

--- OC.m
+++ OC.m
@@ -4,7 +4,7 @@ l1 = 0; l2 = 100000; move = 0.2;
 while (l2-l1 > 1e-4)
   lmid = 0.5*(l2+l1);
   xnew = max(0.001,max(x-move,min(1.,min(x+move,x.*sqrt(-dc'./lmid)))));
-  if sum(sum(xnew)) - volfrac*N > 0;
+  if sum(xnew) - volfrac*N > 0;
     l1 = lmid;
   else
     l2 = lmid;
