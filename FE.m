734086813b0c99fea1ae622c745568a5a8338c90
Optimisation-Pfe <77837016+Optimisation-Pfe@users.noreply.github.com>
2021-02-10 18:36:54 +0100

    Update FE.m

--- FE.m
+++ FE.m
@@ -5,7 +5,7 @@ function [U]=FE(N2,C,connectiv,x,penal,F,fixeddofs,freedofs)
     U = zeros(N2,1);
     for i=1:C
         e=connectiv(i,:);
-        edof = [2*e(1)-1; 2*e(1);2*e(4)-1; 2*e(4); 2*e(3)-1; 2*e(3); 2*e(2)-1; 2*e(2)];
+        edof = [2*e(1)-1; 2*e(1);2*e(2)-1; 2*e(2); 2*e(3)-1; 2*e(3); 2*e(4)-1; 2*e(4)];
         K(edof,edof) = K(edof,edof) + x(i)^penal*KE;
     end
     % SOLVING
